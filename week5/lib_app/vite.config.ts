1675ddfa7d23f3da580961dd001b75a9fb8c411a
yuxian9542 <zyuxian9542@gmail.com>
2025-10-14 22:28:15 -0400

    add work path to vite to find index.html'

--- week5/lib_app/vite.config.ts
+++ week5/lib_app/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
     open: true
