50045af1da99129d04fa716279b18d9c09de78e9
yuxian9542 <zyuxian9542@gmail.com>
2025-10-14 22:41:22 -0400

    reove root from vite.config.ts

--- week5/lib_app/vite.config.ts
+++ week5/lib_app/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './',
+//   root: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
